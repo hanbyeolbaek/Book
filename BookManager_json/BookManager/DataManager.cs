cf9c8e2dbdc030afcc409bf64d0a8f192e823c86
hbbaek <hb.baek@gmail.com>
2020-05-12 10:45:17 +0900

    도서관리 프로그램 - json 파일로 데이터 관리 - json 파일은 book과 user로 도서과 사용자 별도 관리 - json 파일이 존재하지 않을 경우 파일 생성 - txt 파일에 기능별 버튼을 누를때마다 누른 시간과 로그 기록 - txt 파일도 book과 user로 별도 관리 < form1 > - 도서 빌리기 - 도서 반납하기 < form2 > - 도서 추가 - 도서 수정 - 도서 삭제 < form3 > - 유저 추가 - 유저 수정 - 유저 삭제

---
+++ BookManager_json/BookManager/DataManager.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BookManager
+{
+    class DataManager
+    {
+        public static List<Book> Books = new List<Book>();
+        public static List<User> Users = new List<User>();
+        static string jsonFileBooks = "./Books.json";
+        static string jsonFileUsers = "./Users.json";
+
+        static DataManager()
+        {
+            JsonLoad();
+        }
+
+        public static void JsonLoad()
+        {
+            FileInfo fileInfo = new FileInfo(jsonFileBooks);
+            if (fileInfo.Exists)
+            {
+                string stBookValueJson = File.ReadAllText(@jsonFileBooks);
+                JObject jsonObjectBook = JObject.Parse(stBookValueJson);
+                Books = (from item in jsonObjectBook["books"]["book"]
+                         select new Book()
+                         {
+                             Isbn = item["isbn"].ToString(),
+                             Name = item["name"].ToString(),
+                             Publisher = item["publisher"].ToString(),
+                             Page = int.Parse(item["page"].ToString()),
+                             BorrowedAt = DateTime.Parse(item["borrowedAt"].ToString()),
+                             isBorrowed = item["isBorrowed"].ToString() == "1" ? true : false,
+                             UserId = int.Parse(item["userId"].ToString()),
+                             UserName = item["userName"].ToString()
+                         }).ToList<Book>();
+            }
+            else
+            {
+                BooksCreateFile();
+                SaveJson();
+                JsonLoad();
+            }
+
+            fileInfo = new FileInfo(jsonFileUsers);
+            if (fileInfo.Exists)
+            {
+                string stUserValueJson = File.ReadAllText(@jsonFileUsers);
+                JObject jsonObjectUser = JObject.Parse(stUserValueJson);
+                Users = (from item in jsonObjectUser["users"]["user"]
+                         select new User()
+                         {
+                             Id = int.Parse(item["id"].ToString()),
+                             Name = item["name"].ToString()
+                         }).ToList<User>();
+            }
+            else
+            {
+                UsersCreateFile();
+                SaveJson();
+                JsonLoad();
+            }
+        }
+
+        public static void BooksCreateFile()
+        {
+            string booksFileName = @jsonFileBooks;
+            StreamWriter textWrite = File.CreateText(booksFileName);
+            textWrite.Dispose();
+        }
+
+        public static void UsersCreateFile()
+        {
+            string usersFileName = jsonFileUsers;
+            StreamWriter textWrite = File.CreateText(usersFileName);
+            textWrite.Dispose();
+        }
+
+        public static void SaveJson()
+        {
+            var jBoookArray = new JArray();
+            foreach(var item in Books)
+            {
+                var jBookobject = new JObject();
+                jBookobject.Add("isbn", item.Isbn);
+                jBookobject.Add("name", item.Name);
+                jBookobject.Add("publisher", item.Publisher);
+                jBookobject.Add("page", item.Page);
+                jBookobject.Add("borrowedAt", item.BorrowedAt.ToLongDateString());
+                jBookobject.Add("isBorrowed", item.isBorrowed);
+                jBookobject.Add("userId", item.UserId);
+                jBookobject.Add("userName", item.UserName);
+                jBoookArray.Add(jBookobject);
+            }
+
+            var jBookArrayObject = new JObject();
+            jBookArrayObject.Add("book", jBoookArray);
+
+            var jBooksObject = new JObject();
+            jBooksObject.Add("books", jBookArrayObject);
+
+            var jUserArray = new JArray();
+            foreach(var item in Users)
+            {
+                var jUserObject = new JObject();
+                jUserObject.Add("id", item.Id);
+                jUserObject.Add("name", item.Name);
+                jUserArray.Add(jUserObject);
+            }
+
+            var jUserArrayObject = new JObject();
+            jUserArrayObject.Add("user", jUserArray);
+
+            var jUsersObject = new JObject();
+            jUsersObject.Add("users", jUserArrayObject);
+
+            //저장
+            File.WriteAllText(@"./Books.json", jBooksObject.ToString());
+            File.WriteAllText(@"./Users.json", jUsersObject.ToString());
+        }
+    }
+}
---
+++ BookManager_json/BookManager/Form3.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManager
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            Text = "사용자관리";
+
+            dataGridView_Users.DataSource = DataManager.Users;
+            dataGridView_Users.CurrentCellChanged += DataGridView_Users_CurrentCellChanged;
+
+            //람다 button_Add 동작
+            button_Add.Click += (sender, e) =>
+            {
+                try
+                {
+                    if(DataManager.Users.Exists((x) => x.Id == int.Parse(textBox_ID.Text)))
+                    {
+                        MessageBox.Show("사용자 ID가 겹칩니다.");
+
+                        TextFile.UsersHistory("사용자 ID 중복", "추가");
+                    }
+                    else
+                    {
+                        if (textBox_ID.Text.Trim() == "")
+                        {
+                            MessageBox.Show("사용자 ID를 입력해주세요.");
+
+                            TextFile.UsersHistory("사용자 ID 미입력", "추가");
+                        }
+                        else if (textBox_Name.Text.Trim() == "")
+                        {
+                            MessageBox.Show("사용자의 이름을 입력해주세요.");
+
+                            TextFile.UsersHistory("사용자 이름 미입력", "추가");
+                        }
+                        else
+                        {
+                            User user = new User()
+                            {
+                                Id = int.Parse(textBox_ID.Text),
+                                Name = textBox_Name.Text
+                            };
+                            DataManager.Users.Add(user);
+
+                            dataGridView_Users.DataSource = null;
+                            dataGridView_Users.DataSource = DataManager.Users;
+                            DataManager.SaveJson();
+
+                            MessageBox.Show($"사용자 \"{user.Name}\" 님이 추가되었습니다.");
+
+                            TextFile.UsersHistory($"{user.Name}", "추가");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            };
+
+            button_Modify.Click += (sender, e) =>
+            {
+                try
+                {
+                    if (textBox_Name.Text.Trim() == "")
+                    {
+                        MessageBox.Show("사용자의 이름을 입력해주세요.");
+
+                        TextFile.UsersHistory("사용자 이름 미입력", "수정");
+                    }
+                    else
+                    {
+                        User user = DataManager.Users.Single((x) => x.Id == int.Parse(textBox_ID.Text));
+                        user.Name = textBox_Name.Text;
+
+                        MessageBox.Show($"\"{user.Name}\" 으로 이름이 변경되었습니다.");
+
+                        TextFile.UsersHistory($"{user.Name}", "수정");
+                    }
+
+                    try
+                    {
+                        Book book = DataManager.Books.Single((x) => x.UserId == int.Parse(textBox_ID.Text));
+                        book.UserName = textBox_Name.Text;
+                    }
+                    catch (Exception)
+                    {
+                        
+                    }
+
+                    dataGridView_Users.DataSource = null;
+                    dataGridView_Users.DataSource = DataManager.Users;
+                    DataManager.SaveJson();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("존재하지 않는 사용자입니다.");
+                    
+                    TextFile.UsersHistory("존재하지 않는 사용자", "수정");
+                }
+            };
+
+            button_Delete.Click += (sender, e) =>
+            {
+                try
+                {
+                    User user = DataManager.Users.Single((x) => x.Id == int.Parse(textBox_ID.Text));
+
+                    MessageBox.Show($"사용자 \"{user.Name}\" 님이 삭제되었습니다.");
+
+                    TextFile.UsersHistory($"{user.Name}", "삭제");
+
+                    DataManager.Users.Remove(user);
+
+                    dataGridView_Users.DataSource = null;
+                    dataGridView_Users.DataSource = DataManager.Users;
+                    DataManager.SaveJson();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("존재하지 않는 사용자입니다.");
+
+                    TextFile.UsersHistory("존재하지 않는 사용자", "삭제");
+                }
+            };
+        }
+
+        private void DataGridView_Users_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                User user = dataGridView_Users.CurrentRow.DataBoundItem as User;
+                textBox_ID.Text = user.Id.ToString();
+                textBox_Name.Text = user.Name;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void textBox_ID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //숫자만 입력되도록 필터링
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))    //숫자와 백스페이스를 제외한 나머지를 바로 처리
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
